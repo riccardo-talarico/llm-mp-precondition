5fcb446d98dd1c1fe8771bbc9bf84773810ccb87
riccardo-talarico <riccardotalarico363@gmail.com>
2026-02-20 10:52:27 +0100

    Added aliases for hallucinated keys

--- utils/graph.py
+++ utils/graph.py
@@ -1,25 +1,28 @@
 from langgraph.graph import StateGraph
 from typing_extensions import TypedDict, Literal, List
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, AliasChoices
 from IPython.display import Image
 
 # TODO: add __str__ methods for all the structured output, to make it more readable for the LLM 
 class GoPrimitive(BaseModel):
-    name : str
-    type : str
-    function : str
-    scope : str
+    name : str = Field(alias="Name")
+    type : str = Field(alias = "Type")
+    function : str = Field(alias = "Function")
+    scope : str = Field(alias = "Scope")
 
 class GoPrimitives(BaseModel):
-    primitives : List[GoPrimitive] | None
+    primitives : List[GoPrimitive] | None = Field(validation_alias=AliasChoices("concurrency_primitives", "Primitives", "Concurrency_primitives"))
+    class Config:
+        # This allows to use GoPrimitives(primitives=...) in the code
+        populate_by_name = True
 
 class ActionStep(BaseModel):
-    goroutine : str
-    action : str
+    goroutine : str = Field(alias="Routine")
+    action : str = Field(alias = "Action")
 
 class Trace(BaseModel):
-    interleaving_logic : str 
-    sequence : List[ActionStep]
+    interleaving_logic : str = Field(validation_alias=AliasChoices("Logic","logic","Interleaving_logic"))
+    sequence : List[ActionStep] = Field(alias="Sequence")
 
 class BugClassification(BaseModel):
     subtype : Literal["Resource Deadlock", "Communication Deadlock", "Mixed Deadlock"]
