79f18edfaad441ab342840be90195d7e098384bb
riccardo-talarico <riccardotalarico363@gmail.com>
2026-03-31 11:36:58 +0200

    Added try_to_invoke with ouput fixing and token usage

--- agent/verificationAgent.py
+++ agent/verificationAgent.py
@@ -5,22 +5,24 @@ load_dotenv()
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_groq import ChatGroq
-from langchain.messages import HumanMessage, SystemMessage, AIMessage
+from langchain.messages import HumanMessage, SystemMessage
+
+from utils.graph import BugClassification
+from utils.output_parser import try_to_invoke
 from utils.tool_analysis import log_tool_interactions
 from utils.prompts import SINGLE_PROMPT_DETECTION_AND_CLASSIFICATION
 from utils.results import extract_id, get_usage_metadata, try_into_dataframe, print_token_count
-import pandas as pd
 
 
 class VerificationAgent():
 
-  def __init__(self, validation_paths: str, test_paths : str,provider='Google',model="gemini-2.5-flash", propose_fix=False, logging = True):
+  def __init__(self, validation_paths: str, test_paths : str,provider='Google',model="gemini-2.5-flash", logging = True):
     if provider == 'Google':
       api_key = os.getenv("GEMINI_API_KEY")
       self.llm = ChatGoogleGenerativeAI(model=model,google_api_key=api_key, max_retries = 3)
     elif provider == 'Groq':
       api_key = os.getenv('GROQ_API_KEY')
-      self.llm = ChatGroq(model=model, groq_api_key=api_key)
+      self.llm = ChatGroq(model=model, groq_api_key=api_key, max_retries=1)
     else:
       print("Other providers not currently supported")
       self.llm = None  
@@ -30,34 +32,7 @@ class VerificationAgent():
     self.model = model
     self.usage_metadata = []
     self.last_run_time = -1
-
-    # Defining JSON response schema
-    properties_schemas = {}
-    properties_schemas['classification'] = {
-      'type': "object",
-      'description':"Provide a classification for the bug (if there is one) found in the code based on the hierarchy provided. In case there is no bug, put 'None' for all the required fields",
-      'properties': {
-        'class':{'type':'string','description':'Class of the bug (blocking or nonblocking)'},
-        'type':{'type':'string','description':'Type of the bug, chosen according to the classification hierarchy'},
-        'subtype':{'type':'string','description':'Subtype of the bug, chosen according to the classification hierarchy'}
-      },
-      'required':['class','subtype','type']
-    }
-    self.response_schema = {
-      "type": "object",
-      "description": "Response schema for the verification",
-      "properties" : properties_schemas,
-      'required': list(properties_schemas.keys())
-    }
-    if propose_fix:
-      self.response_schema['properties']['proposed_fix']={
-        'type': ['string','null'],
-        'description':'Suggest eventual fixes to the program to make it satisfy all properties. Must be shown as git diffs.',
-        'examples': ['-ch:=make(chan) +ch:=make(chan,1)']
-      }
-      self.response_schema['required'].append('proposed_fix')
-    
-    self.agent = self.llm.with_structured_output(self.response_schema, method="json_mode", include_raw=True)
+    self.structured_llm = self.llm.with_structured_output(BugClassification, include_raw=True)
       
   def run_on_benchmark(self, validation : bool = False, save_usage_metadata : bool=True, insert_sleep:bool=True):
     
@@ -70,7 +45,7 @@ class VerificationAgent():
     thinking_log = {}
     verified_prg = 0
     start = time.time()
-    for prg_path in prg_paths[:1]:
+    for prg_path in prg_paths:
       with open(prg_path[:-1], "r") as f:
         id = extract_id(prg_path[:-1])
         print(f"Id: {id}")
@@ -79,7 +54,7 @@ class VerificationAgent():
         default_response = {
           'class':'None','type': 'None', 'subtype': 'None'
         }
-        response = self.try_to_invoke(messages, default_response)
+        response = try_to_invoke(self.structured_llm,messages,BugClassification,self.llm, default_response)
         try:
           print(f"{response['parsed']}")
           print(f"{response['raw']}")
