0e3e1c1106a8e920dcedef96fedef84bc4019679
riccardo-talarico <riccardotalarico363@gmail.com>
2026-04-01 11:23:21 +0200

    Fixed bug on saving benchmark and added Ollama

--- agent/verificationAgent.py
+++ agent/verificationAgent.py
@@ -5,24 +5,33 @@ load_dotenv()
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_groq import ChatGroq
+from langchain_ollama import ChatOllama
 from langchain.messages import HumanMessage, SystemMessage
 
 from utils.graph import BugClassification
 from utils.output_parser import try_to_invoke
 from utils.tool_analysis import log_tool_interactions
 from utils.prompts import SINGLE_PROMPT_DETECTION_AND_CLASSIFICATION
+from utils.experiments import OllamaExperimentConfig
 from utils.results import extract_id, get_usage_metadata, try_into_dataframe, print_token_count
 
 
 class VerificationAgent():
 
-  def __init__(self, validation_paths: str, test_paths : str,provider='Google',model="gemini-2.5-flash", logging = True):
+  def __init__(self, validation_paths: str, test_paths : str,provider='Google',model="gemini-2.5-flash", logging = True,ollama_cfg : None | OllamaExperimentConfig = None):
     if provider == 'Google':
       api_key = os.getenv("GEMINI_API_KEY")
       self.llm = ChatGoogleGenerativeAI(model=model,google_api_key=api_key, max_retries = 3)
     elif provider == 'Groq':
       api_key = os.getenv('GROQ_API_KEY')
       self.llm = ChatGroq(model=model, groq_api_key=api_key, max_retries=1)
+    elif provider == 'Ollama':
+      self.llm = ChatOllama(
+          base_url=ollama_cfg.base_url,
+          model=ollama_cfg.model,
+          temperature=ollama_cfg.temperature,
+          model_kwargs={"seed": ollama_cfg.seed, **ollama_cfg.options}
+      )
     else:
       print("Other providers not currently supported")
       self.llm = None  
@@ -82,22 +91,10 @@ class VerificationAgent():
     if self.logging:
       log_tool_interactions(response)
 
-    res = try_into_dataframe(classification_data, a.model)
+    res = try_into_dataframe(classification_data, self.model)
 
     return res
   
-  def try_to_invoke(self, msg, default_response):
-    while True:
-      try:
-        response = self.agent.invoke(msg)
-        return response
-      except Exception as e:
-        print(f"Error while invoking the model {e}")
-        s = input("Return default response? [Y/N]")
-        if s == 'Y':
-          return default_response
-        
-
     
 #Groq models: qwen/qwen3-32b, llama-3.3-70b-versatile, llama-3.1-8b-instant, meta-llama/llama-4-maverick-17b-128e-instruct,
 # moonshotai/kimi-k2-instruct-0905
