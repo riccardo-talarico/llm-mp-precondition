8810f95647c2fc24c6158c99a8e40852b474da55
riccardo-talarico <riccardotalarico363@gmail.com>
2026-04-27 10:30:22 +0200

    Add prompt versioning and new saving system

--- utils/experiments.py
+++ utils/experiments.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass
-from typing import Any
 from pathlib import Path
-import os, yaml, requests
+from datetime import datetime
+from typing import Any, Dict
+import os, yaml, requests, csv, json, uuid, re
+from pydantic import BaseModel
+import pandas as pd
 
 @dataclass
 class OllamaExperimentConfig:
@@ -10,9 +13,24 @@ class OllamaExperimentConfig:
     model: str
     temperature: float
     seed: int
+    _set : str
     warmup: bool
     paths_file: str
     options: dict[str, Any]
+    prompt_config: dict[str, str]
+
+    def extract_metadata(self):
+        #TODO: add section for prompt config
+        return {
+            "model": self.model,
+            'set': self._set, 
+            "temperature": self.temperature, 
+            "top_p": self.options.get("top_p"), 
+            "top_k": self.options.get("top_k"),
+            "num_ctx": self.options.get("num_ctx")
+        }
+    
+
 
 def load_config(config_path: Path) -> OllamaExperimentConfig:
     with config_path.open("r", encoding="utf-8") as f:
@@ -26,9 +44,11 @@ def load_config(config_path: Path) -> OllamaExperimentConfig:
         model=raw["model"],
         temperature=raw.get("temperature", 0.0),
         seed=raw.get("seed", 42),
+        _set = raw.get("set","validation"),
         warmup=bool(raw.get("warmup", True)),
         options=raw.get("options", {}),
-        paths_file=f'benchmarks_paths/{str(raw.get("set","validation"))}_set.txt'
+        paths_file=f'benchmarks_paths/{str(raw.get("set","validation"))}_set.txt',
+        prompt_config=raw.get("prompt_config",{})
     )
 
 
@@ -47,6 +67,108 @@ def do_warmup(base_url: str, model: str, timeout_sec: int, options: dict[str, An
         "options": options,
     }
     try:
+        print(f"Calling: {base_url}/api/generate", flush=True)
         post_json(f"{base_url}/api/generate", payload, timeout_sec)
     except Exception as e:
         print(f"Warm-up failed for {model}: {e}")
+
+
+class ExperimentLogger:
+    def __init__(self, base_path: str = "results"):
+        self.base_path = base_path
+        self.classification_dir = os.path.join(base_path, "classifications")
+        self.index_path = os.path.join(base_path, "master_index.csv")
+        os.makedirs(self.classification_dir, exist_ok=True)
+        
+        # Initialize CSV if it doesn't exist
+        if not os.path.exists(self.index_path):
+            with open(self.index_path, 'w', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(["run_id", "timestamp", "architecture", "set", "model", "temperature", "top_p", "top_k", "num_ctx","seed", "prompt_v", "output_file"])
+
+    def log_run(self, metadata: Dict[str, Any], output_df: pd.DataFrame):
+        run_id = str(uuid.uuid4())[:8]
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        filename = f"run_{run_id}.csv"
+        file_path = os.path.join(self.classification_dir, filename)
+
+        # TODO: save also the raw outputs of the complete chain?
+
+        # Saving only the classification
+        output_df.to_csv(file_path, index=True)
+
+        # Updates the searchable table
+        with open(self.index_path, 'a', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow([
+                run_id, 
+                timestamp, 
+                metadata.get("architecture"),
+                metadata.get('set'),
+                metadata.get("model"), 
+                metadata.get("temperature"), 
+                metadata.get("top_p"), 
+                metadata.get("top_k"),
+                metadata.get("num_ctx"),
+                metadata.get("seed"),
+                metadata.get("prompt_v"),
+                filename
+            ])
+        
+        print(f"Experiment {run_id} logged to {self.index_path}")
+
+
+def get_prompt_version(prompt_name: str, version: str | None = None):
+    directory = f"prompts/{prompt_name}"
+    if not os.path.exists(directory):
+        raise FileNotFoundError(f"Directory not found: {directory}")
+    if not version:
+        version = extract_latest_prompt_version(prompt_name)
+        ## Get all .yaml files with their full paths
+        #files = [
+        #    os.path.join(directory, f) 
+        #    for f in os.listdir(directory) 
+        #    if f.endswith(('.yaml', '.yml'))
+        #]
+        #if not files:
+        #    raise FileNotFoundError(f"No YAML files found in {directory}")
+#
+        ## Get the file with the most recent modification time
+        #latest_file = max(files, key=os.path.getmtime)
+        #return latest_file
+    
+
+    for filename in os.listdir(directory):
+        if version in filename and filename.endswith(('.yaml', '.yml')):
+            return os.path.join(directory, filename)
+
+    raise ValueError(f"Could not find version '{version}' in {directory}")
+
+
+def extract_latest_prompt_version(prompt_name:str):
+    directory = f"prompts/{prompt_name}"
+    if not os.path.exists(directory):
+        raise FileNotFoundError(f"Directory not found: {directory}")
+   
+    # Get all .yaml files with their full paths
+    files = [
+        os.path.join(directory, f) 
+        for f in os.listdir(directory) 
+        if f.endswith(('.yaml', '.yml'))
+    ]
+    if not files:
+        raise FileNotFoundError(f"No YAML files found in {directory}")
+
+    # Get the file with the most recent modification time
+    latest_file = max(files, key=os.path.getmtime)
+    match = re.search(r"_v(\d+)", latest_file)
+    return str(match.group(0))[1:]
+
+def load_prompt_string(file_path: str) -> str:
+    """
+    Reads a YAML file and returns the raw template string.
+    """
+    with open(file_path, 'r', encoding='utf-8') as f:
+        config = yaml.safe_load(f)
+    
+    return config.get("template", "")
